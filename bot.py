3b5d4674fa735e18debbe849a8364cdcaa9349ef
Karim Shoorbajee <karim.shoorbajee@thinkingwithtools.com>
2019-08-23 23:10:43 -0400

    refactoring so as not to  get null pointers (this is python but you know what I mean)

--- bot.py
+++ bot.py
@@ -52,48 +52,6 @@ c_gms = {}
 c_gms_md = {}
 while True:
 
-    #get xml with current week game data
-    resp = requests.get("http://www.nfl.com/liveupdate/scorestrip/ss.xml")
-    with open('week_games.xml', 'wb') as f:
-        f.write(resp.content)
-
-    #parse it 
-    tree = ET.parse('week_games.xml')
-    root = tree.getroot()
-
-    #check if there are new games
-    games = root.find('gms')
-    if games.attrib['w'] != c_week:
-        # new gameweek!
-        # empty the current games directory and put all the new games in there.
-        c_week_gms = {}
-        c_week = games.attrib['w']
-        print("this should only run once")
-
-    # update the game statuses for this week (quarter, time, etc)
-    for g in games:
-        c_week_gms[g.attrib['eid']] = g.attrib
-    
-    # update the dictionary of currently played games
-    for g in c_week_gms:
-        if c_week_gms[g]['q'].isdigit() or c_week_gms[g]['q'] == "H":
-            # if the quarter is a digit (1,2,3,4,5) that means there's a quarter in progress (the game isn't over/has started/is not in halftime)
-            if not g in c_gms.keys():
-                c_gms[g] = c_week_gms[g]
-                print("new game added")
-                print(c_gms[g]['h'])
-                print(c_gms[g]['v'])
-        else:
-            # if the quarter is not a digit that means the game is not in play and thus we remove it from the dictionary of currently playing games
-            if g in c_gms.keys():
-                c_gms[g] = None
-                c_gms_md[g] = None
-
-    # make a Game object for each game that is currently being played.
-    for g in c_gms:
-        if not g in c_gms_md.keys():
-            c_gms_md[g] = game.Game(g)
-    
     # check if there have been any punts and if there have been tweet about it!
     for g in c_gms_md:
         resp = requests.get(c_gms_md[g].url)
@@ -144,6 +102,50 @@ while True:
                         round(sindex,2)
                     )
                 )
+                
+    #get xml with current week game data
+    resp = requests.get("http://www.nfl.com/liveupdate/scorestrip/ss.xml")
+    with open('week_games.xml', 'wb') as f:
+        f.write(resp.content)
+
+    #parse it 
+    tree = ET.parse('week_games.xml')
+    root = tree.getroot()
+
+    #check if there are new games
+    games = root.find('gms')
+    if games.attrib['w'] != c_week:
+        # new gameweek!
+        # empty the current games directory and put all the new games in there.
+        c_week_gms = {}
+        c_week = games.attrib['w']
+        print("this should only run once")
+
+    # update the game statuses for this week (quarter, time, etc)
+    for g in games:
+        c_week_gms[g.attrib['eid']] = g.attrib
+    
+    # update the dictionary of currently played games
+    for g in c_week_gms:
+        if c_week_gms[g]['q'].isdigit() or c_week_gms[g]['q'] == "H":
+            # if the quarter is a digit (1,2,3,4,5) that means there's a quarter in progress (the game isn't over/has started/is not in halftime)
+            if not g in c_gms.keys():
+                c_gms[g] = c_week_gms[g]
+                print("new game added")
+                print(c_gms[g]['h'])
+                print(c_gms[g]['v'])
+        else:
+            # if the quarter is not a digit that means the game is not in play and thus we remove it from the dictionary of currently playing games
+            if g in c_gms.keys():
+                c_gms[g] = None
+                c_gms_md[g] = None
+
+    # make a Game object for each game that is currently being played.
+    for g in c_gms:
+        if not g in c_gms_md.keys():
+            c_gms_md[g] = game.Game(g)
+    
+    
     time.sleep(60)
 
     
